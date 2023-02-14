e7036c679b9468bda5046c4e4cdddb3b845b5f04
TA1-SA <118293853+TA1-SA@users.noreply.github.com>
2023-02-14 16:56:14 +0300

    Add files via upload

--- HR User.sql
+++ HR User.sql
@@ -54,9 +54,7 @@ order by 3 desc
 select employee_id ,first_name ,salary ,salary *1.10 from employees 
 where salary between 10000 and 20000
 order by 3 desc 
-
 --ranges between 10,000 and 20,000
-
 ==================================================================
 select employee_id ,first_name ,salary ,salary *1.10 from employees 
 where first_name between 'B' and 'D'
@@ -74,3 +72,50 @@ and  hire_date <='31-DEc-07'
 
 --whose hired in 2007
 ===========================================
+select * from employees
+where last_name like 'A%'
+
+
+--whose last name start with A
+
+=========================================
+
+
+select * from employees
+where last_name like '%a'
+
+
+--whose last name end with a
+===========================================
+select * from employees
+where last_name like '____'
+
+
+--whose last name is 4 letter length 
+=============================================
+select * from employees
+where last_name like '__a%'
+
+--whose last name third character is a 
+==============================================
+select * from employees
+
+where salary >=5000 and (department_id =10 or department_id=30 or department_id=50)
+
+
+--whose salary is 5000 or more  and his department is 10 or 30  or 50 
+
+--priority  ()  and   or 
+
+
+
+
+
+
+
+
+
+
+
+
+
